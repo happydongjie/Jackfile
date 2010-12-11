3aedc50207951edf525b483a6dd4d801fa2e76ac
Jack Zhang <zhangdongjie@ubuntu.(none)>
2010-12-11 11:35:13 +0800

    包含汉语注示

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(int argc, const char *argv[])
 {
-    int int_a=97;
-    printf("char :%d\n",int_a);
+    int int_a=97;//定义变量
+    printf("char :%d\n",int_a);//输出整型
     printf("int :%c\n",int_a); 
     printf("0x :%x\n",int_a);
     printf("sizeof=%d\n",sizeof(int_a));
